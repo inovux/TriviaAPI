72e63dc2b7a7836a50b91489b42f6941c8866a41
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-06-17 19:18:12 +0800

    chore: Removed comment that was not needed

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -103,11 +103,6 @@ class TriviaTestCase(unittest.TestCase):
         self.assertEqual(data['error'], 404)
         self.assertEqual(data['message'], 'resource not found')
 
-    """
-    TODO
-    Write at least one test for each test for successful operation and for expected errors.
-    """
-
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
