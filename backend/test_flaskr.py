2c1639bf84b41ea82b85fc19a65672265bb0a3e4
Stanley Thijssen <stanley.thijssen@gmail.com>
2020-06-19 21:26:02 +0800

    feat: Added test for search term without results

--- backend/test_flaskr.py
+++ backend/test_flaskr.py
@@ -112,6 +112,15 @@ class TriviaTestCase(unittest.TestCase):
         self.assertTrue(data['total_questions'])
         self.assertEqual(len(data['questions']), 8)
 
+    def test_get_question_by_search_term_without_results(self):
+        res = self.client().post('/questions/search', json={'search_term': 'Bla bla bla'})
+        data = json.loads(res.data)
+
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(data['success'], True)
+        self.assertEqual(data['total_questions'], 0)
+        self.assertEqual(len(data['questions']), 0)
+
 # Make the tests conveniently executable
 if __name__ == "__main__":
     unittest.main()
